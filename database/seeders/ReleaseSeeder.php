78bd8c90606a35875d5d0b79dec3508c98769f98
fermi-paradox <86816389+the-fermi-paradox@users.noreply.github.com>
2024-09-18 14:20:41 -0500

    Removes unnecessary use

--- database/seeders/ReleaseSeeder.php
+++ database/seeders/ReleaseSeeder.php
@@ -2,8 +2,6 @@
 declare(strict_types=1);
 namespace Database\Seeders;
 
-use database\seeders\JsonSeeder;
-
 class ReleaseSeeder extends JsonSeeder
 {
     protected string $filePath = 'data/releases.json';
