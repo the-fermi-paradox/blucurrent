f6210c0506d21923936b3d64e918be5a3215c0f4
fermi-paradox <86816389+the-fermi-paradox@users.noreply.github.com>
2024-09-18 14:56:06 -0500

    Adds one-to-many relationship between release and empire

--- app/Models/Empire.php
+++ app/Models/Empire.php
@@ -3,7 +3,10 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Empire extends Model
 {
+    public function release() : BelongsTo {
+        return $this->belongsTo(Release::class);
+    }
 }
--- app/Models/Release.php
+++ app/Models/Release.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Release extends Model
 {
-
+    public function empires() : HasMany {
+        return $this->hasMany(Empire::class);
+    }
 }
