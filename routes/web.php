87282582aa641bbfdf5438616af9f05b06d29503
fermi-paradox <86816389+the-fermi-paradox@users.noreply.github.com>
2024-09-22 14:32:01 -0500

    Reorder routes

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,11 @@ declare(strict_types=1);
 use App\Http\Controllers\EmpireController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{col?}/{order?}', [EmpireController::class, 'index'])->name('empire.list');
+Route::get('/empire/create/', [EmpireController::class, 'create'])->name('empire.create');
+Route::post('/empire/create/', [EmpireController::class, 'store'])->name('empire.store');
 Route::delete('/empire/delete/{id}', [EmpireController::class, 'destroy'])->name('empire.delete');
 
 Route::put('/empire/update/{id}', [EmpireController::class, 'update'])->name('empire.update');
 Route::get('/empire/update/{id}', [EmpireController::class, 'edit'])->name('empire.edit');
 
-Route::get('/empire/create/', [EmpireController::class, 'create'])->name('empire.create');
-Route::post('/empire/create/', [EmpireController::class, 'store'])->name('empire.store');
+Route::get('/{col?}/{order?}', [EmpireController::class, 'index'])->name('empire.list');
