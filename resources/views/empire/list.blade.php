e88fc512e6e88411fe80e98c2ce23b9d74eb876c
fermi-paradox <86816389+the-fermi-paradox@users.noreply.github.com>
2024-09-22 14:08:53 -0500

    Adds favicon.ico support

--- resources/views/empire/list.blade.php
+++ resources/views/empire/list.blade.php
@@ -16,6 +16,11 @@
         rel="stylesheet"
     />
     @vite(['resources/js/app.js', 'resources/css/app.css'])
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
+
 </head>
 <body>
 <div class="gradient-custom-2 h-100">
